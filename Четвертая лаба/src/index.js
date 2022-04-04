e7d572cf619c57381f308b5426d624991cd9c85a
Alanhis <alanhis043@gmail.com>
2022-04-04 20:53:02 +0500

    Изменение языка ant-а

--- Четвертая лаба/src/index.js
+++ Четвертая лаба/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Lab.js';
-
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/lib/locale/ru_RU';
 
 ReactDOM.render(
   <React.StrictMode>
+   <ConfigProvider locale={ruRU}>
     <App />
+   </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
